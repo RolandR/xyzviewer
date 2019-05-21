bf9a4a6519de580847b6493f75f526ecebd162a8
Roland Rytz <roland.rytz@gmail.com>
2019-05-21 22:41:53 +0200

    make controls actually do stuff

--- _js/xyzviewer.js
+++ _js/xyzviewer.js
@@ -1,20 +1,52 @@
 
 
-function handleFile(files){
-	var reader = new FileReader;
-	reader.onloadend = function(e){
-		processFile(e.target.result);
-	};
-	reader.readAsText(files[0]);
+document.getElementById("files").addEventListener("submit", handleFile);
+
+function handleFile(event){
+	
+	event.preventDefault();
+	
+	var file = document.getElementById("file").files[0];
+	
+	var fileName = file.name.split(".");
+	var extension = fileName[fileName.length-1];
+	
+	var reader = new FileReader();
+	
+	if(extension == "xyz"){
+		reader.onloadend = function(e){
+			processXyzFile(e.target.result);
+		};
+		reader.readAsText(file);
+	} else if(extension == "asc"){
+		reader.onloadend = function(e){
+			processAscFile(e.target.result);
+		};
+		reader.readAsText(file);
+	} else {
+		console.log("File extension is "+extension+", but only xyz and asc are known.");
+	}
+	
+	return false;
 }
 
-function processFile(file){
-	console.log('Loaded file, processing...');
+function processAscFile(file){
 	console.log('File length: '+file.length);
+	console.log('Trimming...');
 	file = file.trim();
+	
+	console.log(file.slice(0, 1000));
+}
+
+function processXyzFile(file){
+	console.log('File length: '+file.length);
+	console.log('Trimming...');
+	file = file.trim();
+	console.log('Splitting...');
 	var flatMap = file.split(/[\s]+/);
 	var entriesCount = flatMap.length;
 	console.log('List has '+entriesCount+' entries.');
+	
 	var maximumX = flatMap[0];
 	var maximumY = flatMap[1];
 	var maximumZ = flatMap[2];
@@ -23,6 +55,7 @@ function processFile(file){
 	var minimumY = flatMap[1];
 	var minimumZ = flatMap[2];
 	console.log('Finding extreme values...');
+	
 	for(var i = 0; i < flatMap.length; i += 3){
 			maximumX = Math.max(maximumX, flatMap[i]);
 			minimumX = Math.min(minimumX, flatMap[i]);
@@ -55,40 +88,52 @@ function processFile(file){
 	context.fillStyle = "rgba(0, 255, 0, 0.5)";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	console.log('Drawing... (This might take a while)');
+	console.log('Filling array...');
 	
-	var color = 0;
+	var floatArray = new Float32Array(canvas.width*canvas.height);
+	
+	for(var i = 2; i < entriesCount; i += 3){
+		var height = (flatMap[i] - minimumZ)/zRange;
+		var x = Math.floor(((flatMap[i-2] - minimumX)/scale));
+		var y = Math.floor(((maximumY - flatMap[i-1])/scale));
+		var pos = canvas.width*y + x;
+		
+		floatArray[pos] = height;
+	}
+	
+	console.log('Drawing...');
 	
 	var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 	var data = imageData.data;
 	//var newImage = context.createImageData(imageData);
 	
-	for(var i = 2; i < entriesCount; i += 3){
-		color = Math.floor(((flatMap[i] - minimumZ)/zRange)*255);
-		var x = Math.floor(((flatMap[i-2] - minimumX)/scale));
-		var y = Math.floor(((maximumY - flatMap[i-1])/scale));
-		var pos = (imageData.width*y + x)*4;
+	if(!document.getElementById("shading").checked){
+	
+		for(var i in floatArray){
+			var color = Math.floor(floatArray[i]*256);
+			
+			data[i*4  ] = color;
+			data[i*4+1] = color;
+			data[i*4+2] = color;
+			data[i*4+3] = 255;
+		}
 		
-		data[pos  ] = color;
-		data[pos+1] = color;
-		data[pos+2] = color;
-		data[pos+3] = 255;
+		context.putImageData(imageData, 0, 0);
+	
+	} else {
+	
+		for(var i in floatArray){
+			var gradient = (floatArray[i] - floatArray[i-1 - canvas.width])*10 + 0.5;
+			var color = Math.floor(gradient*256);
+			
+			data[i*4  ] = color;
+			data[i*4+1] = color;
+			data[i*4+2] = color;
+			data[i*4+3] = 255;
+		}
+		
+		context.putImageData(imageData, 0, 0);
 	}
-	
-	context.putImageData(imageData, 0, 0);
-	
-	return;
-	
-	var foo = context.getImageData(0, 0, canvas.width, canvas.height).data;
-	
-	for(var i = 0; i < foo.length; i += 4){
-		var gradient = (foo[i] - foo[i-4 - 4*canvas.width])*10 + 128 + foo[i]*0.2;
-		data[i  ] = gradient;
-		data[i+1] = gradient;
-		data[i+2] = gradient;
-	}
-	
-	context.putImageData(imageData, 0, 0);
 	
 	console.log('Done.');
 	
